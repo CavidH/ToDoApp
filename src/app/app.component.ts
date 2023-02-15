a6cbbddf8def655d5b62f4adffac123bbe975f83
Cavid Haciyev <javid.hajiyev@ziraatbank.az>
2023-02-15 10:18:48 +0400

    :cactus:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoApp';
+
+  getTodos(): ToDo[] {
+    return [{
+      id: 1,
+      content: "sdsd",
+      status: true,
+    },{
+      id: 1,
+      content: "sdsd",
+      status: true,
+    },{
+      id: 1,
+      content: "sdsd",
+      status: true,
+    },{
+      id: 1,
+      content: "sdsd",
+      status: true,
+    }]
+  }
 }
 
 class ToDo {
-  
+  id: number;
+  content: string;
+  status: boolean;
 }
